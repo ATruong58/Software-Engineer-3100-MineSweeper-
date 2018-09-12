5cf9b84cdcc2384f1b5fe6ecc439c8d4e9cbd501
Koch <etk34b@rc13xcs213.managed.mst.edu>
2018-09-11 20:39:59 -0500

    Creates Board

--- minesweeperMain.cpp
+++ minesweeperMain.cpp
@@ -13,23 +13,24 @@ Minesweeper Implementation
 using namespace std;
 
 //Board Size: Medium = 16x16
-const int BSIZE = 17;
+const int BSIZE = 18;
 //Number of Bombs: Meduim = 40
 const int NBOMBS = 40;
 
-void createBoard(char b[][BSIZE]);
-void addBombs(char b[][BSIZE]);
-void addNumbers(char b[][BSIZE]);
+void createBoard(char b[BSIZE][BSIZE]);
+void addBombs(char b[BSIZE][BSIZE]);
+void addNumbers(char b[BSIZE][BSIZE]);
+void print(char b[BSIZE][BSIZE]);
 
 int main()
 {
-  char mediumBoard[17][17];
+  char mediumBoard[BSIZE][BSIZE] = {};
   createBoard(mediumBoard);
-  cout << "Error?" << endl;
+  print(mediumBoard);
   return 0;
 }
 
-void createBoard(char b[][BSIZE])
+void createBoard(char b[BSIZE][BSIZE])
 {
   for(int x = 0; x <= BSIZE; x++)
   {
@@ -42,18 +43,19 @@ void createBoard(char b[][BSIZE])
   addNumbers(b);
 }
 
-void addBombs(char b[][BSIZE])
+void addBombs(char b[BSIZE][BSIZE])
 {
 
 }
 
-void addNumbers(char b[][BSIZE])
+void addNumbers(char b[BSIZE][BSIZE])
 {
   int numBombs = 0;
-  for(int x = 1; x < BSIZE; x++)
+  for(int x = 1; x < BSIZE -1; x++)
   {
-    for(int y = 1; y < BSIZE; y++)
+    for(int y = 1; y < BSIZE -1; y++)
     {
+
       //sets the number of surrounding bombs to 0 for each square
       numBombs = 0;
       //If current square is a Bomb, Don't Do Anything
@@ -70,8 +72,24 @@ void addNumbers(char b[][BSIZE])
         if(b[x + 1][y] == 'b') numBombs ++;//Right Neighbor
         if(b[x + 1][y + 1] == 'b') numBombs ++;//Bottom Right Neighbor
         //Sets the current Square Equal to the number of bombs around it
-        b[x][y] = numBombs;
+        switch(numBombs)
+        {
+          case 0: b[x][y] = '-';
+                  break;
+        }
       }
     }
+  }
+}
+
+void print(char b[BSIZE][BSIZE])
+{
+  for(int x = 0; x < BSIZE; x++)
+  {
+    for(int y = 0; y < BSIZE; y++)
+    {
+      cout << b[x][y] << " ";
+    }
+    cout << endl;
   }
 }
